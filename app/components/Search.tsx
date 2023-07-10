0f728588016e689c933de91513007b76058ec443
Grégory Marini <greg.marini@hotmail.fr>
2023-07-10 11:12:39 +0200

    add searchBox in Navbar

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -6,7 +6,7 @@ export default function Search() {
   const [search, setSearch] = useState('');
   const router = useRouter();
 
-  const handleSubmit = async (e: FormEvent) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setSearch('');
     router.push(`/${search}/`);
