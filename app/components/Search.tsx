39cca86ded6d40acecb985a1d823b8daf6136009
Grégory Marini <greg.marini@hotmail.fr>
2023-07-11 14:01:54 +0200

    build data page(Item)

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -8,8 +8,8 @@ export default function Search() {
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setSearch('');
     router.push(`/${search}/`);
+    setSearch('');
   };
   return (
     <form
