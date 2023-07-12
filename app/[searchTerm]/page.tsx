18ea5979f403b39f1bd3c988304b06fc44550b90
Grégory Marini <greg.marini@hotmail.fr>
2023-07-12 11:38:31 +0200

    add error page for [searchTerm] folder

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -31,7 +31,7 @@ export default async function SearchResults({ params: { searchTerm } }: Props) {
   const results: Result[] | undefined = data?.query?.pages;
 
   const content = (
-    <main className='bg-slate-200 mx-auto max-w-lg py-1 min-h-screen'>
+    <main className='bg-slate-200 mt-[1rem] mx-auto max-w-lg py-1 min-h-screen'>
       {results ? (
         Object.values(results).map((result) => {
           return <Item key={result.pageid} result={result} />;
